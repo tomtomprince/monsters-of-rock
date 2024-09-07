d72ec68baefa106b69b74bfb2a62d406f7e99af4
Tom Prince <tprince14@gmail.com>
2024-09-07 11:06:51 -0700

    add all shadcn/ui and use skeleton to test

--- application/README.md
+++ application/README.md
@@ -1,8 +1,9 @@
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
 ## Tech Used
-- https://dashboard.convex.dev/
+- https://dashboard.convex.dev/ - Database, cron, etc
 - https://dashboard.clerk.com/ - Clerk - Auth
+- https://ui.shadcn.com/ - UI Components
 
 ## Getting Started
 
--- application/src/app/events/page.tsx
+++ application/src/app/events/page.tsx
@@ -2,13 +2,16 @@
 
 import { useQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Events() {
     const events = useQuery(api.events.get);
     return (
       <main>
         <h1 className="text-xl">Events List</h1>
-        {!events && <div>Loading...</div>}
+        {!events && (
+          <Skeleton className="animate-pulse rounded-md bg-muted h-4 w-[250px]" />
+        )}
         {events &&
           events?.map(({ _id, title, description }) => (
             <div key={_id}>
